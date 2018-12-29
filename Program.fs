2a47941f501f681bf3e8379dfac33062d68ea393
Mendel Monteiro <mendel.monteiro@gmail.com>
2018-12-29 18:22:47 +0100

    Part One

---
+++ Program.fs
@@ -0,0 +1,14 @@
+﻿
+open System
+open System.IO
+
+let partOne =
+    File.ReadAllLines(__SOURCE_DIRECTORY__ + "\input1.txt") 
+    |> Seq.map (fun x -> Int32.Parse(x))
+    |> Seq.sum
+
+
+[<EntryPoint>]
+let main argv =
+    printfn "Hello World from F#!"
+    0
