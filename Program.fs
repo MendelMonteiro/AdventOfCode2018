92a641cd8d87c94e344d6bdc6607c5c0831dc887
Mendel Monteiro <mendel.monteiro@gmail.com>
2018-12-30 17:20:30 +0100

    Part Two

--- Program.fs
+++ Program.fs
@@ -7,6 +7,19 @@ let partOne =
     |> Seq.map (fun x -> Int32.Parse(x))
     |> Seq.sum
 
+let partTwo = 
+    let inputLines = File.ReadAllLines(__SOURCE_DIRECTORY__ + "\input1.txt") |> Array.ofSeq
+    let findDuplicate (existing:Set<'a>, found) x = if existing.Contains x then (existing, Some x) else (x |> existing.Add, None)
+
+    let result = Seq.initInfinite (fun x -> inputLines)
+                 |> Seq.concat
+                 |> Seq.map (fun x -> Int32.Parse(x))
+                 |> Seq.scan (+) 0
+                 |> Seq.scan (fun s x -> findDuplicate s x) (Set.empty, None)
+                 |> Seq.filter (fun (_, x) -> Option.isSome x)
+                 |> Seq.head
+    
+    printfn "%A" result
 
 [<EntryPoint>]
 let main argv =
