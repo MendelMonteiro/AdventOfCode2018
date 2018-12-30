8398f1dcc9d022a746918672b4db817cfaab1382
Mendel Monteiro <mendel.monteiro@gmail.com>
2018-12-30 21:36:40 +0100

    Day 2 - Part One

--- Program.fs
+++ Program.fs
@@ -2,12 +2,12 @@
 open System
 open System.IO
 
-let partOne =
+let puzzle1Part1 =
     File.ReadAllLines(__SOURCE_DIRECTORY__ + "\input1.txt") 
     |> Seq.map (fun x -> Int32.Parse(x))
     |> Seq.sum
 
-let partTwo = 
+let puzzle1Part2 = 
     let inputLines = File.ReadAllLines(__SOURCE_DIRECTORY__ + "\input1.txt") |> Array.ofSeq
     let findDuplicate (existing:Set<'a>, found) x = if existing.Contains x then (existing, Some x) else (x |> existing.Add, None)
 
@@ -19,6 +19,31 @@ let partTwo =
                  |> Seq.filter (fun (_, x) -> Option.isSome x)
                  |> Seq.head
     
+    printfn "%A" result
+
+let puzzle2Part1 =
+    let inputLines = File.ReadAllLines(__SOURCE_DIRECTORY__ + "\input2.txt") |> Array.ofSeq
+    let accDuplicates (two, three) (_, i) =
+        match i with
+        | 2 -> (1, three)
+        | 3 -> (two, 1)
+        | _ -> (two, three)
+
+    let identifyDuplicate (s:string) = 
+        s 
+        |> List.ofSeq
+        |> List.groupBy (fun x -> x) 
+        |> List.map (fun (x, xs) -> (x, Seq.length xs)) 
+        |> List.fold accDuplicates (0, 0)
+    
+    let result = 
+        let multiply (x, y) = x * y
+        let sum (currTwo, currThree) (two, three) = (currTwo + two, currThree + three)
+        inputLines
+        |> Seq.map identifyDuplicate
+        |> Seq.fold sum (0, 0)
+        |> multiply
+
     printfn "%A" result
 
 [<EntryPoint>]
