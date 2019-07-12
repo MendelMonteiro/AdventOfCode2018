596e4a36474414ec1500843c816e7789dc1daad8
Mendel Monteiro <mendel.monteiro@gmail.com>
2019-07-12 07:29:31 +0200

    Day 8 - Part Two

--- Puzzle8.fs
+++ Puzzle8.fs
@@ -15,7 +15,7 @@ type Metadata = int
 type Number = int
 type Node = {Children: Node list; Data: Metadata list}
 
-let toNumbers (i : string) = i.Split(' ') |> Array.map Int32.Parse 
+let toNumbers (i : string) : Number list = i.Split(' ') |> Array.map Int32.Parse |> List.ofArray
 let readMetadata m r = r |> List.take m
 let skipMetadata m r = r |> List.skip m
 
@@ -41,7 +41,31 @@ let rec parseNode numbers currentLevelNodeCount : (Node list * Number list)=
             ([{Children = children; Data = readMetadata m rest'}], rest' |> skipMetadata m)
 
 let collectData node = 
-    let rec traverseDataCore node = node.Data :: (List.collect traverseDataCore node.Children) 
-    traverseDataCore node |> List.concat
+    let rec collectDataCore node = node.Data :: (List.collect collectDataCore node.Children) 
+    collectDataCore node |> List.concat
 
-parseNode (input |> toNumbers |> List.ofArray) 1 |> fst |> List.head |> collectData |> List.sum 
+let sumData = collectData >> List.sum
+let rootNode (x, _) = x |> List.head
+
+// 2 3 0 3 10 11 12 1 1 0 1 99 2 1 1 2
+// A----------------------------------
+//     B----------- C-----------
+//                      D-----
+
+let puzzle8Part1 = 
+    parseNode (input |> toNumbers) 1 |> rootNode |> sumData
+
+let puzzle8Part2 = 
+    let rec sumLeafNodes (node:Node) = 
+        let sumChildNode (nodes:Node list) s x  = 
+            match x with
+            | 0 -> s
+            | d when d <= nodes.Length -> s + (sumLeafNodes (nodes |> List.item (d - 1)))
+            | _ -> s 
+        match node.Children with
+        | [] -> // No children, sum metadata
+            node.Data |> List.sum
+        | c -> // Has children, check metadata to try to index into children
+            node.Data |> List.fold (sumChildNode c) 0
+
+    parseNode (input |> toNumbers) 1 |> rootNode |> sumLeafNodes 
